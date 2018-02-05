a99f9f431bb09f2db70d242bb8f1ae62379d4151
Tristian M <tgmorale@ucsc.edu>
2018-02-04 21:28:35 -0800

    improved CombatManager

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -4,8 +4,8 @@ using System.Collections;
 
 public class CombatManager : MonoBehaviour
 {
-	public enum ACTION{THROW, CATCH, GATHER, SKILL, REST}
-	public enum PHASE{START, SELECT, ACTION, TARGET, EXECUTE, RESULTS}
+	public enum ACTION{NONE, THROW, CATCH, GATHER, SKILL, REST}
+	public enum PHASE{START, CONFLICT, SELECT, ACTION, TARGET, EXECUTE, RESULTS}
 
 	public struct Combat{public Character character; 
 						public ACTION action; 
@@ -22,118 +22,135 @@ public class CombatManager : MonoBehaviour
 	// This is for debug purposes
 	public Text battleText;										
 	// true if the player has to choose turn order.
-	public Character selection;	
+	public int selection;	
 	public ACTION actionSelection;
 
 	// Used to see if balls were caught and by who
 	private System.Collections.Generic.List<bool> ballsCaught;
+	private System.Collections.Generic.List<int> conflictInQueue;
 	public bool win = false;
 	public bool lose = false;
 
 	void Start()
 	{
-		//selection = null;
-		//actionSelection = null;
+		selection = -1;
 		Player = new Character[3];
-		Player[0] = GameObject.Find ("Player").GetComponent<Character>();
-		Player[1] = GameObject.Find ("Player (1)").GetComponent<Character>();
-		Player[2] = GameObject.Find ("Player (2)").GetComponent<Character>();
 		Enemy = new Character[3];
-		Enemy[0] = GameObject.Find ("Enemy").GetComponent<Character>();
-		Enemy[1] = GameObject.Find ("Enemy (1)").GetComponent<Character>();
-		Enemy[2] = GameObject.Find ("Enemy (2)").GetComponent<Character>();
 		playerSelect = new Button [3];
-		playerSelect [0] = GameObject.Find ("Player").GetComponent<Button>();
-		playerSelect[1] = GameObject.Find ("Player (1)").GetComponent<Button>();
-		playerSelect[2] = GameObject.Find ("Player (2)").GetComponent<Button>();
-		playerSelect [0].onClick.AddListener (()=>CharacterSelect (Player[0]));
-		playerSelect [1].onClick.AddListener (()=>CharacterSelect (Player[1]));
-		playerSelect [2].onClick.AddListener (()=>CharacterSelect (Player[2]));
 		enemySelect = new Button [3];
-		enemySelect [0] = GameObject.Find ("Enemy").GetComponent<Button>();
-		enemySelect[1] = GameObject.Find ("Enemy (1)").GetComponent<Button>();
-		enemySelect[2] = GameObject.Find ("Enemy (2)").GetComponent<Button>();
-		enemySelect [0].onClick.AddListener (()=>CharacterSelect (Enemy[0]));
-		enemySelect [1].onClick.AddListener (()=>CharacterSelect (Enemy[1]));
-		enemySelect [2].onClick.AddListener (()=>CharacterSelect (Enemy[2]));
+		for (int i = 0; i < 3; i++) 
+		{
+			Player[i] = GameObject.Find ("Player"+i).GetComponent<Character>();
+			Enemy[i] = GameObject.Find ("Enemy"+i).GetComponent<Character>();
+
+			playerSelect [i] = GameObject.Find ("Player"+i).GetComponent<Button>();
+			playerSelect [i].onClick.AddListener (()=>CharacterSelect (i));
+			enemySelect [i] = GameObject.Find ("Enemy"+i).GetComponent<Button>();
+			enemySelect [i].onClick.AddListener (()=>CharacterSelect (i));
+		}
 		action = new Button[1];
 		action [0] = GameObject.Find ("AttackButton").GetComponent<Button> ();
-		action[0].onClick.AddListener (()=>ActionSelect(ACTION.THROW));		
+		action[0].onClick.AddListener (()=>ActionSelect(0));		
 		battleText = GameObject.Find ("BattleText").GetComponent<Text> ();
 		//action[1].onClick.AddListener (()=>ActionSelect(ACTION.CATCH));
 		//action[2].onClick.AddListener (()=>ActionSelect(ACTION.GATHER));
 		//action[3].onClick.AddListener (()=>ActionSelect(ACTION.SKILL));
 		//action[4].onClick.AddListener (()=>ActionSelect(ACTION.REST));
 		ballsCaught = new System.Collections.Generic.List<bool>();
+		conflictInQueue = new System.Collections.Generic.List<int>();
 		combatQueue = new Combat[6];
 	}
 
 	void Update()
 	{
+		// Currently the coin toss is not implememted
+
 		switch (currentPhase) 
 		{
-		case(PHASE.START):						// Currently the coin toss is not implememted
+		case(PHASE.START):
 			StartQueue ();
-			currentPhase = PHASE.SELECT;
+			break;
+		case(PHASE.CONFLICT):
+			Conflict();
 			break;
 		case(PHASE.SELECT):
 			Select ();
-			currentPhase = PHASE.EXECUTE;
+			break;
+		case(PHASE.ACTION):
+			Action ();
+			break;
+		case(PHASE.TARGET):
+			Target ();
 			break;
 		case(PHASE.EXECUTE):
 			Execute ();
-			currentPhase = PHASE.RESULTS;
 			break;
 		case(PHASE.RESULTS):
 			Results ();
-			currentPhase = PHASE.START;
 			break;
 		}
 	}
 
 	void StartQueue()								
 	{
-		int j = 0;
-		int k = 0;
-		for(int i = 0; i < 6; i++)
+		Debug.Log ("StartQueue(), Phase: " + currentPhase);
+		Character[] tempChar = new Character[6];
+		for (int i = 0; i < 3; i++) 
 		{
-			// If either array actually gets to the end this causes a problem
-			if (j < 3) {
-				Debug.Log (Player [j].Stamina);
-				Debug.Log (Enemy [j].Stamina);
+			tempChar [i] = Player [i];
+			tempChar [i + 3] = Enemy [i];
+		}
 
-				if (k > 2 || Player [j].Stamina >= Enemy [k].Stamina) {
-					combatQueue [i].character = Player [j];
-					j++;
-					print ("Character " + i + " is " + combatQueue [i].character.Name);
-				}
-			}
-			if (k < 3) {
-				Debug.Log ("in k");
-				if (j > 2 || Player [j].Stamina < Enemy [k].Stamina) {
-					combatQueue [i].character = Enemy [k];
-					k++;
-					print ("Character " + i + " is " + combatQueue [i].character.Name);
+		for (int i = 0; i < 6; i++) 
+		{
+			for (int j = i+1; j < 6; j++) 
+			{
+				if (tempChar [j-1].Stamina < tempChar [j].Stamina) 
+				{
+					Character temp = tempChar [j];
+					tempChar [j] = tempChar [j - 1];
+					tempChar [j - 1] = temp;
 				}
 			}
 		}
+
+		for (int i = 0; i < 6; i++) 
+		{
+			combatQueue [i].character = tempChar [i];
+			Debug.Log("Player " + i + " is " + combatQueue[i].character.name);
+		}
+
+		for(int i = 0; i < 5; i++)
+		{
+			if (combatQueue [i].character.tag == combatQueue [i].character.tag
+			   && combatQueue [i].character.Stamina == combatQueue [i].character.Stamina) 
+			{
+				conflictInQueue.Add (i);
+			}
+		}
+		currentPhase = PHASE.CONFLICT;
 	}
 
+
+	// This function calls the character select and action select functions in order
+	// pre: combatQueue is in stamina order and all elements have a character
+	// post: each element of CombatQueue has a character, action, and target
+	/*
 	void Select()
 	{
+		Debug.Log ("Select(), Phase: " + currentPhase);
 		for (int i = 0; i < 6; i++) 
 		{
 			if(! combatQueue[i].character.dead)
 			{
-				if (combatQueue [i].character.tag == "player") 
+				if (combatQueue [i].character.tag == "Player") 
 				{
-					if (i != 5) 
+					if (i != 5 && combatQueue [i + 1].character.tag == "Player") 
 					{
-						if (combatQueue [i + 1].character.tag == "player") 
-						{
-							PlayerChoice (i);
-						}
+						conflictInQueue = true;
+						PlayerChoice (i);
 					}
+					currentPhase = PHASE.ACTION;
 					ActionChoice (i);
 				} 
 				else 
@@ -163,97 +180,110 @@ public class CombatManager : MonoBehaviour
 				}
 				combatQueue [j] = temp;
 				j++;
-				// How does garbage collection work in C# for Unity? 
 			}
 		}
+		currentPhase = PHASE.EXECUTE;
 	}
+	*/
 
-	void EnemyTurn(int current)
-	{
-		int choice = Random.Range (0, 4);
-		switch (choice) 
-		{
-		case(0):
-			combatQueue [current].action = ACTION.THROW;
-			break;
-		case(1):
-			combatQueue [current].action = ACTION.CATCH;
-			break;
-		case(2):
-			combatQueue [current].action = ACTION.GATHER;
-			break;
-		case(3):
-			combatQueue [current].action = ACTION.SKILL;
-			break;
-		}
-		if (combatQueue [current].action != ACTION.GATHER) 
-		{
-			choice = Random.Range (0, 3);
-			while (Player [choice].dead) 
-			{
-				choice = Random.Range (0, 3);
-			}
-			combatQueue [current].target = Player [choice];
-		}
-	}
 
-	// This is meant to "pause" void Select until the player selects a character
-	// it probably doesn't work right
-	void PlayerChoice(int first)
+	// This phase is used to check if there are still conflicts in the queue and set current phase to the correct phase
+	void Conflict()
 	{
-		if(currentPhase == PHASE.SELECT)
+		Debug.Log ("Conflict(), Phase: " + currentPhase);
+		foreach(Button B in enemySelect)
 		{
-			while (selection == null) 
-			{}
-			while(! selection == combatQueue[first].character && ! selection == combatQueue[first+1].character)
-			{}
-			if (selection == combatQueue [first + 1].character) 
+			B.enabled = false;
+		}
+		int firstAction = -1;
+		for (int i = 5; i > -1; i--)
+		{
+			if (combatQueue [i].action == ACTION.NONE) 
 			{
-				Character temp = combatQueue [first].character;
-				combatQueue [first].character = combatQueue [first + 1].character;
-				combatQueue [first + 1].character = temp;
+				firstAction = i;
 			}
 		}
-	}
-
-	void ActionChoice(int first)
-	{
-		if(currentPhase == PHASE.SELECT)
+		// If there is a conflict in the queue but someone else has priority
+		if (conflictInQueue.Count > 0 && firstAction > conflictInQueue [0]) 
 		{
-			while (actionSelection == null) //Change this
-			{}
-			if (currentPhase == PHASE.TARGET && combatQueue [first].action != ACTION.GATHER) 
+			currentPhase = PHASE.SELECT;
+		} 
+		else 
+		{
+			if(!combatQueue[firstAction].character.dead)
 			{
-				currentPhase = PHASE.TARGET;
-				while (selection != null) 
-				{}
-				while (selection.tag != "Enemy") 
-				{}
-				combatQueue [first].target = selection;
-				currentPhase = PHASE.SELECT;
-			} 
-			else 
-			{
-				combatQueue [first].target = null;
+				if (combatQueue [firstAction].character.tag == "Player") 
+				{
+					currentPhase = PHASE.ACTION;
+				} 
+				else 
+				{
+					EnemyTurn (firstAction);
+				}
 			}
+			else
+			{
+				for (int i = firstAction; i < 6; i++) 
+				{
+					combatQueue [i].action = ACTION.REST;
+				}
+			}
+		}		
+		if (firstAction == -1) 
+		{
+			// This block does a stable sort to move the players who are catching to the front of the queue
+			int j = 0;
+			for (int i = 0; i < 6; i++) 
+			{
+				int k = 0;
+				if(combatQueue[i].action == ACTION.CATCH)
+				{
+					Combat temp = combatQueue [i];
+					while (i - k > j) 
+					{
+						combatQueue [i - k] = combatQueue [i - k - 1];
+						k++;
+					}
+					combatQueue [j] = temp;
+					j++;
+				}
+			}
+			currentPhase = PHASE.EXECUTE;
 		}
 	}
 
-	// This is where the code for selecting a character with the mouse goes.
-	void CharacterSelect(Character character)
+
+	void Select()
 	{
-		selection = character;
+		Debug.Log ("Select(), Phase: " + currentPhase);
+		// delegates would help here
+		combatQueue [conflictInQueue [0]].character.gameObject.GetComponent<Button> ().enabled = true;
+		combatQueue [conflictInQueue [0]+1].character.gameObject.GetComponent<Button> ().enabled = true;
+		// If the player has selected a character to go first
 	}
 
-	// This is where the code for selecting an action with the mouse goes.
-	void ActionSelect(ACTION action)
+	void Action()
 	{
-		actionSelection = action;
+		Debug.Log ("Action(), Phase: " + currentPhase);
+		foreach(Button B in playerSelect)
+		{
+			B.enabled = false;
+		}
+	}
+
+	void Target ()
+	{
+		Debug.Log ("Target(), Phase: " + currentPhase);
+		foreach (Button B in enemySelect) 
+		{
+			B.enabled = true;
+		}
 	}
 
 	// This function has the charcters perform their actions in the correct order
 	void Execute()
 	{
+		Debug.Log ("Execute(), Phase: " + currentPhase);
 		for (int i = 0; i < 6; i++) 
 		{
 			switch (combatQueue [i].action) 
@@ -291,7 +321,9 @@ public class CombatManager : MonoBehaviour
 				break;
 			}
 		}
+		currentPhase = PHASE.RESULTS;
 	}
+
 
 	// This function checks to see if either team lost, heals any benched players, and rezs in that order.
 	void Results()
@@ -318,7 +350,137 @@ public class CombatManager : MonoBehaviour
 			Resurrect (ballsCaught[0]);
 			ballsCaught.RemoveAt (0);
 		}
+		currentPhase = PHASE.START;
 	}
+
+	public void CharacterSelect(int character)
+	{
+		if (currentPhase == PHASE.SELECT) 
+		{
+			if (Player [character] != combatQueue [conflictInQueue [0]].character) 
+			{
+				Character temp = combatQueue [conflictInQueue [0]].character;
+				combatQueue [conflictInQueue [0]].character = combatQueue [conflictInQueue [0] + 1].character;
+				combatQueue [conflictInQueue [0] + 1].character = temp;
+			}
+			conflictInQueue.RemoveAt (0);
+			currentPhase = PHASE.ACTION;
+		}
+		if (currentPhase == PHASE.TARGET) 
+		{
+			int firstAction = -1;
+			for(int i = 5; i > -1; i--)
+			{
+				if(combatQueue[i].action == ACTION.NONE)
+				{
+					firstAction = i;
+				}
+			}
+			combatQueue [firstAction].target = Enemy [firstAction];
+			currentPhase = PHASE.CONFLICT;
+		}
+	}
+
+	public void ActionSelect(int action)
+	{
+		int firstAction = -1;
+		for(int i = 5; i > -1; i--)
+		{
+			if(combatQueue[i].action == ACTION.NONE)
+			{
+				firstAction = i;
+			}
+		}
+		switch (action) 
+		{
+		case(0):
+			combatQueue [firstAction].action = ACTION.THROW;
+			break;
+		}
+		if (combatQueue [firstAction].action == ACTION.THROW) 
+		{
+			currentPhase = PHASE.TARGET;
+		} 
+		else 
+		{
+			currentPhase = PHASE.CONFLICT;
+		}
+	}
+
+	void EnemyTurn(int current)
+	{
+		int choice = Random.Range (0, 4);
+		switch (choice) 
+		{
+		case(0):
+			combatQueue [current].action = ACTION.THROW;
+			break;
+		case(1):
+			combatQueue [current].action = ACTION.CATCH;
+			break;
+		case(2):
+			combatQueue [current].action = ACTION.GATHER;
+			break;
+		case(3):
+			combatQueue [current].action = ACTION.SKILL;
+			break;
+		}
+		if (combatQueue [current].action != ACTION.GATHER) 
+		{
+			choice = Random.Range (0, 3);
+			while (Player [choice].dead) 
+			{
+				choice = Random.Range (0, 3);
+			}
+			combatQueue [current].target = Player [choice];
+		}
+	}
+
+	/*
+	// This is meant to "pause" void Select until the player selects a character
+	// it probably doesn't work right
+	void PlayerChoice(int first)
+	{
+		Debug.Log ("PlayerChoice(), Phase: " + currentPhase);
+		//Debug.Log ("PlayerChoice(" + first + "), currentPhase = " + currentPhase);
+		battleText.text = "Choose Player " + first + " or Player " + (first + 1);
+		if(currentPhase == PHASE.SELECT)
+		{
+			combatQueue [first].character.gameObject.GetComponent<Button> ().enabled = true;
+			combatQueue [first+1].character.gameObject.GetComponent<Button> ().enabled = true;
+			// makes sure selection is one of the two characters.
+			//while(conflictInQueue)
+			//{Debug.Log ("in THE loop");}
+			if (combatQueue[selection].character == combatQueue [first + 1].character) 
+			{
+				Character temp = combatQueue [first].character;
+				combatQueue [first].character = combatQueue [first + 1].character;
+				combatQueue [first + 1].character = temp;
+			}
+			combatQueue [first].character.gameObject.GetComponent<Button> ().enabled = false;
+			combatQueue [first+1].character.gameObject.GetComponent<Button> ().enabled = false;
+			conflictInQueue = false;
+		}
+	}
+	*/
+
+
+
+	/*
+	// This is where the code for selecting a character with the mouse goes.
+	public void CharacterSelect(int character)
+	{
+		selection = character;
+		conflictInQueue = false;
+		Debug.Log ("Character " + character + " pressed");
+	}
+	*/
+
+
+
+
+
+
 
 	// This function puts players back into the game
 	void Resurrect(bool player)
