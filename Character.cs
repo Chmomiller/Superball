741c77e655b74ba7af6b9a29f0fd369ea4777833
Shadovar <carlerez@yahoo.com>
2018-02-17 14:25:13 -0800

    Updated Character and TemporaryUIIntegration
    Added fields to Character to store target, selected action, and type of action.

---
+++ Character.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    public string Name = "default";
+	public int Damage = 1;
+    public int Catch = 100;
+    public int Gather = 0;
+    public int Capacity = 4;
+    public int Stamina = 10;
+	public int maxStamina = 10;
+    public int heldBalls = 0;
+    public string Role = "Supporter";
+	public Character Target; //Create an empty Character in the combat manager that other charaters can select when not targetting
+	public string action = "";
+	public string actionType = "";
+	public int targetingType = 0; //0 for self/predetermined, 1 for enemies, 2 for allies
+	//Following 3 arrays are paired, so when an action is selected it can look at the same index
+	//of the different arrays and set the corresponding variable to the array value
+	private string[] actions = {"Catch", "Throw", "Gather", "Skill1", "Skill2", "Skill3" };
+	private string[] actionTypes = { "Defense", "Offense", "Utility", "Utility", "Utility" };
+	private int[] targetingTypes = { 0, 1, 0, 0, 0 };
+
+    public bool dead = false;
+	public bool catching = false;
+
+
+    public void loseStamina(int staminaLoss) { this.Stamina -= staminaLoss; }
+    public void gainStamina(int staminaLoss) { this.Stamina += staminaLoss; }
+
+	public virtual int catchBall(int damage, Character attacker)
+	{
+		int caught = 0;
+		if (this.catching) 
+		{
+			if ((Random.Range (1, 100) + Random.Range (1, 100) / 2) < this.Catch) 
+			{ // you can catch it
+				this.catching = false;
+				if (this.heldBalls < Capacity) 
+				{
+					this.heldBalls++;
+				}
+				caught = 1;
+			}
+		}
+		if(this.Stamina > this.Stamina/2)
+		{
+			loseStamina (damage);
+			return caught;
+		}
+		else
+		{
+			 
+			if ((Random.Range (1, 100) + Random.Range (1, 100) / 2) < (this.Stamina/this.maxStamina) * 100) 
+			{//dodge success
+				loseStamina (1);
+				return 0;
+			} 
+			else 
+			{
+				if (this.Stamina <= 0)
+					this.dead = true; 
+				loseStamina (1);
+				return 0;
+			}
+		}
+    }
+
+	public int throwBall(Character target)
+	{
+		heldBalls--;
+		return target.catchBall (this.Damage, target);
+	}
+
+    public void Rest()
+	{
+		this.gainStamina (1);
+    }
+
+	public void gatherBall()
+	{
+		heldBalls += Gather;
+		if(heldBalls > Capacity)
+		{
+			heldBalls = Capacity;
+		}
+	}
+
+    public virtual void Skill1(){
+
+    }
+
+    public virtual void Skill2(){
+
+    }
+
+    public virtual void Skill3(){
+
+    }
+
+    public void Skill4(){
+
+    }
+}
---
+++ TemporaryUIIntegration.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TemporaryUIIntegration : MonoBehaviour 
+{
+	public Text Name;
+	public Character Player;
+	public Slider HealthBar;
+	float HealthMax;
+	// Use this for initialization
+	void Start () 
+	{
+		Name = gameObject.GetComponent<Text> ();
+		Player = gameObject.GetComponent<Character> ();
+		Name.text = Player.Name;
+		HealthBar = gameObject.GetComponent<Slider> ();
+		HealthMax = Player.Stamina;
+		HealthBar.value = Player.Stamina;
+	}
+	
+	// Update is called once per frame
+	//can be optimized by making a bool needsUpdate, setting it to true when stamina changes,
+	//and then having update check needsUpdate rather than calculating
+	void Update () 
+	{
+		HealthBar.value = Player.Stamina;
+		if (HealthBar.value / HealthMax <= .5) {
+			//HealthBar.colors = new ColorBlock (1f, 0f, 0f); //Set to Red
+		} else if (HealthBar.value / HealthMax > .5) {
+			//HealthBar.colors = new ColorBlock (0f, 1f, 0f); //Set to Green
+		}
+	}
+}
