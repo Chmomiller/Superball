0ca4ad2338c69b1fae8204f096a09c59a4ca54eb
Thelonius Erickson <theoserickson@gmail.com>
2018-04-12 02:32:25 -0700

    First Decent Prototype! Added in audio resources and more!
    REMOVED TransitionScene. This is now MainMenu. Added in all the audio from the drive thus far for ease of use. We have done great work to get this far and hopefuly this build will allow the design team to playtest effectively. That said, we still have a lot of work ahead of us but I am proud of what we have done. FYI, nothing happens if you click the dodge ball in the title.

---
+++ Assets/changeSprite.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class changeSprite : MonoBehaviour {
+
+    public Sprite salt;
+    public Sprite schola;
+    public Sprite might;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Change(string filePath) {
+        print(filePath);
+        if (filePath == "salt") {
+            GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = salt;
+        } else if (filePath == "schola") {
+            GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = schola;
+        } else if (filePath == "might") {
+            GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = might;
+        } else {
+            GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = null;
+        }
+        //GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = UnityEditor.AssetDatabase.LoadAssetAtPath<Sprite>(filePath);
+        //GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(filePath);
+        if (Resources.Load<Sprite>(filePath) == null) print("change fail:" +filePath);
+
+    }
+}
