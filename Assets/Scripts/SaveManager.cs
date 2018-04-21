a65368db0bcf374a1d7392463846dd6bba38e57c
Shadovar <carlerez@yahoo.com>
2018-04-20 20:39:23 -0700

    Updated Save Manager
    Save Manager now has Save( ), Read( ), ReadLevels( ), and ReadBooleans( ). Save( ) takes current character levels (and xp once that is being tracked), along with whichever conditions have been completed, and saves them. Read( ) looks at the file where data is saved, and, if there is daved data, changes the variables of the class to the stored variables. ReadLevels( ) and ReadBooleans( ) read the corresponding saved values, and return them in arrays, in case that data is wanted but without overwriting the current data.

--- Assets/Scripts/SaveManager.cs
+++ Assets/Scripts/SaveManager.cs
@@ -46,31 +46,113 @@ public class SaveManager : MonoBehaviour {
         }
 	}
 
-	public void Save(int currLvl, int ShiroLvl, int ShiroExp, int TheoLvl, int TheoExp, int ClemLvl, int ClemExp){
+	public void Save(){
 		FileStream file = File.Open (Application.persistentDataPath + "/info.dat", FileMode.Create);
 		BinaryWriter bf = new BinaryWriter (file);
-		bf.Write ((decimal) currLvl);
-		bf.Write ((decimal) ShiroLvl);
-		bf.Write ((decimal) ShiroExp);
-		bf.Write ((decimal) TheoLvl);
-		bf.Write ((decimal) TheoExp);
-		bf.Write ((decimal) ClemLvl);
-		bf.Write ((decimal) ClemExp);
+		bf.Write ((decimal) shiro.Level);
+		bf.Write ((decimal) theodore.Level);
+		bf.Write ((decimal) clemence.Level);
+
+		bf.Write (SaltPittDialogue);
+		bf.Write (SaltPittBattle);
+		bf.Write (SaltPittBattleHard);
+
+		bf.Write (ScholaGrandisDialog);
+		bf.Write (ScholaGrandisBattle);
+		bf.Write (scholaGrandisBattleHard);
+
+		bf.Write (MightMainDialog);
+		bf.Write (MightMainBattle);
+		bf.Write (MightMainBattleHard);
+
+		bf.Write (yamatoDialog);
+		bf.Write (yamatoBattle);
+		bf.Write (yamatoBattleHard);
+
+		bf.Write (teamSevenDialog);
+		bf.Write (teamSevenBattle);
+
+		bf.Write (beatSuperballRush);
+		bf.Write (unlockAll);
 
 		bf.Close ();
 		file.Close ();
 	}
 
-	public int[] Read(){
-		int[] values = new int[7];
+	public bool Read(){
+		int[] characterLevels = this.ReadLevels ();
+		bool[] conditionsMet = this.ReadBooleans ();
+		if (characterLevels [0] == null || conditionsMet [0] == null) {
+			return false;
+		} else {
+			int shiroTempLvl = characterLevels [0];
+			if (shiroTempLvl > shiro.Level) {
+				shiro.LevelUp (shiroTempLvl - shiro.Level);
+			}
+			int theoTempLvl = characterLevels [1];
+			if (theoTempLvl > theodore.Level) {
+				theodore.LevelUp (theoTempLvl - theodore.Level);
+			}
+			int clemTempLvl = characterLevels [2];
+			if(clemTempLvl > clemence.Level) {
+				clemence.LevelUp (clemTempLvl - clemence.Level);
+			}
+
+			SaltPittDialogue = conditionsMet [0];
+			SaltPittBattle = conditionsMet [1];
+			SaltPittBattleHard = conditionsMet [2];
+
+			ScholaGrandisDialog = conditionsMet [3];
+			ScholaGrandisBattle = conditionsMet [4];
+			scholaGrandisBattleHard = conditionsMet [5];
+
+			MightMainDialog = conditionsMet [6];
+			MightMainBattle = conditionsMet [7];
+			MightMainBattleHard = conditionsMet [8];
+
+			yamatoDialog = conditionsMet [9];
+			yamatoBattle = conditionsMet [10];
+			yamatoBattleHard = conditionsMet [11];
+
+			teamSevenDialog = conditionsMet [12];
+			teamSevenBattle = conditionsMet [13];
+
+			beatSuperballRush = conditionsMet [14];
+			unlockAll = conditionsMet [15];
+		}
+		return true;
+	}
+
+	public int[] ReadLevels(){
+		int[] characterLevels = new int[3];
 		if (File.Exists (Application.persistentDataPath + "/info.dat")) {
 			FileStream file = File.Open (Application.persistentDataPath + "/info.dat", FileMode.Open);
 			BinaryReader br = new BinaryReader(file);
-			for (int i = 0; i < 7; i++) {
-				values [i] = (int) br.ReadDecimal ();
+			for (int i = 0; i < 3; i++) {
+				characterLevels [i] = (int) br.ReadDecimal ();
 			}
+
+			br.Close ();
+			file.Close ();
 		}
-		return values;
+		return characterLevels;
 	}
 		
+	public bool[] ReadBooleans(){
+		bool[] conditionsCompleted = new bool[16];
+		if (File.Exists (Application.persistentDataPath + "/info.dat")) {
+			FileStream file = File.Open (Application.persistentDataPath + "/info.dat", FileMode.Open);
+			BinaryReader br = new BinaryReader(file);
+			for (int i = 0; i < 3; i++) {
+				br.ReadDecimal ();
+			}
+			for (int i = 0; i < 16; i++) {
+				conditionsCompleted [i] = br.ReadBoolean ();
+			}
+
+			br.Close ();
+			file.Close ();
+		}
+		return conditionsCompleted;
+	}
 }
