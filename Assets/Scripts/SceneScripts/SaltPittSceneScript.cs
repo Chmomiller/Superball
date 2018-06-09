df7ecc51f102248d21fd6236f6b6069c10765a0b
Thelonius Erickson <theoserickson@gmail.com>
2018-06-09 13:28:44 -0700

    Changed Audio of Map Screen to credits theme for hype. Buffed up players in Salt Pitt Battle

--- Assets/Scripts/SceneScripts/SaltPittSceneScript.cs
+++ Assets/Scripts/SceneScripts/SaltPittSceneScript.cs
@@ -10,20 +10,20 @@ public class SaltPittSceneScript : MonoBehaviour {
         game = GameObject.Find("GameManager").GetComponent<GameManager>();
         save = GameObject.Find("SaveManager").GetComponent<SaveManager>();
         if (game.hardMode) {
+            GameObject.Find("Character4").GetComponent<Character>().LevelUp(9);
+            GameObject.Find("Character5").GetComponent<Character>().LevelUp(8);
+            GameObject.Find("Character3").GetComponent<Character>().LevelUp(9);
+            GameObject.Find("Character0").GetComponent<Character>().LevelUp(9);
+            GameObject.Find("Character1").GetComponent<Character>().LevelUp(7);
+            GameObject.Find("Character2").GetComponent<Character>().LevelUp(8);
+            print("start hard mode");
+        } else {
             GameObject.Find("Character4").GetComponent<Character>().LevelUp(5);
             GameObject.Find("Character5").GetComponent<Character>().LevelUp(5);
             GameObject.Find("Character3").GetComponent<Character>().LevelUp(5);
             GameObject.Find("Character0").GetComponent<Character>().LevelUp(5);
             GameObject.Find("Character1").GetComponent<Character>().LevelUp(5);
             GameObject.Find("Character2").GetComponent<Character>().LevelUp(5);
-            print("start hard mode");
-        } else {
-            GameObject.Find("Character4").GetComponent<Character>().LevelUp(2);
-            GameObject.Find("Character5").GetComponent<Character>().LevelUp(2);
-            GameObject.Find("Character3").GetComponent<Character>().LevelUp(2);
-            GameObject.Find("Character0").GetComponent<Character>().LevelUp(2);
-            GameObject.Find("Character1").GetComponent<Character>().LevelUp(2);
-            GameObject.Find("Character2").GetComponent<Character>().LevelUp(2);
             print("start regular mode");
         }
 
